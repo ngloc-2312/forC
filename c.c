1eeddf6195723aebf228778e2d2eb9cf17865640
ngducloc <loc23122004@gmail.com>
2024-08-18 23:06:58 +0700

    rename

---
+++ c.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int thang, nam;
+    printf("nhap thang: ");
+    scanf("%d", &thang);
+    if(thang == 4 || thang == 6 || thang == 9 || thang == 11){
+        printf("%d co 30 ngay", thang);
+    }else if(thang == 1 || thang == 3 || thang == 5 || thang == 7 || thang == 8 || thang == 10 || thang == 12){
+        printf("%d co 31 ngay", thang);
+    }
+    if(thang == 2){
+        printf("nhap nam: ");
+        scanf("%d", &nam);
+        if(nam % 400 == 0 || nam % 4 == 0 && nam % 100 != 0){
+             printf("%d co 29 ngay", thang);
+        }else {
+            printf("khong co thang %d", thang);
+        }
+    }
+}
